6984246ccff33c79cef500f59e5f869e48aeb436
pavelbesedin <pavelbesedin97@mail.ru>
2023-04-01 18:29:38 +0300

    Task 8  subTask 3 Найти сотрудника с минимальной зарплатой

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import pro.sky.java.course1.course.Employee;
 public class Main {
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
-        employees[1] = new Employee("a", "b", "c", 1, 35000);
+//        employees[0] = new Employee("a", "b", "c", 1, 35000);
+//        employees[2] = new Employee("a", "b", "c", 1, 32000);
+//        employees[4] = new Employee("a", "b", "c", 1, 42000);
+
 //        employees[2] = new Employee("a", "b", "c", 1, 35000);
 //        getEmployeesData(employees);
-        countMonthSalaryExpenses(employees);
-
+//        countMonthSalaryExpenses(employees);
+//        System.out.println(findEmployeeMinSalary(employees));
     }
     public static void getEmployeesData(Employee[] employeesList) {
         System.out.println("Личный состав:");
@@ -21,17 +24,29 @@ public class Main {
     }
 
 
-    public static void countMonthSalaryExpenses(Employee[] employeesList){
+    public static int countMonthSalaryExpenses(Employee[] employeesList){
         int monthSalary = 0;
         for (int i = 0; i < employeesList.length; i++) {
             if (employeesList[i] != null) {
                 monthSalary += employeesList[i].getSalary();
             }
         }
-            System.out.println("Затраты на зарплату сотрудникам в месяц - " + monthSalary + "руб.");
+        return monthSalary;
         }
 
+    public static int findEmployeeMinSalary(Employee[] employeesList){
+        int minSalary = 0;
+        for (int i = 0; i < employeesList.length; i++) {
+            if(employeesList[i] != null && employeesList[i].getSalary() < minSalary || minSalary == 0 && employeesList[i] != null){
+                minSalary = employeesList[i].getSalary();
+            }
+            else{
 
+            }
+        }
+        return minSalary;
+
+    }
 //    -------------------------------------    class end -------------------------------------
 }
 
