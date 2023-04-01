8745c4b248179c9d739153712d98149a8e33d5e3
pavelbesedin <pavelbesedin97@mail.ru>
2023-04-01 17:08:29 +0300

    created array with employees

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import pro.sky.java.course1.course.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        Employee annaPetrovna = new Employee("Petrova", "Anna", "Petrovna", 2, 3200);
-        Employee annaPetrovna1 = new Employee("Petrova", "Anna", "Petrovna", 4, 3200);
-        Employee annaPetrovna2 = new Employee("Petrova", "Anna", "Petrovna", 1, 3200);
-        Employee annaPetrovna3 = new Employee("Petrova", "Anna", "Petrovna", 3, 3200);
-        System.out.println(annaPetrovna.getDepartments());
+        Employee[] employees = new Employee[10];
     }
+
 }
