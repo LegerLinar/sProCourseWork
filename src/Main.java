d5a2b5238a2c877140543899f27c6805884f013f
pavelbesedin <pavelbesedin97@mail.ru>
2023-04-01 17:26:38 +0300

    Task 8 basic  subtask 1 - Получить список всех сотрудников со всеми имеющимися по ним данными (вывести в консоль значения всех полей (toString)).

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ import pro.sky.java.course1.course.Employee;
 public class Main {
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
+        employees[1] = new Employee("a", "b", "c", 1, 35000);
+        getEmployeesData(employees);
+    }
+    public static void getEmployeesData(Employee[] employeesList){
+        System.out.println("Личный состав:");
+        for (int i = 0; i < employeesList.length; i++) {
+            if (employeesList[i] != null)
+            System.out.println(employeesList[i].toString());
+        }
     }
 
 }
