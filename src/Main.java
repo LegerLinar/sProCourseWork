56e70147090f33da638339e5c6ba823a15aa257f
pavelbesedin <pavelbesedin97@mail.ru>
2023-04-01 20:09:19 +0300

    Task 8  subTask 3 - 4 Refactor return of min and max

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import pro.sky.java.course1.course.Employee;
 public class Main {
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
-//        employees[0] = new Employee("a", "b", "c", 1, 35000);
-//        employees[2] = new Employee("a", "b", "c", 1, 32000);
-//        employees[4] = new Employee("a", "b", "c", 1, 42000);
+        employees[0] = new Employee("a", "b", "c", 1, 35000);
+        employees[2] = new Employee("a", "b", "c", 1, 32000);
+        employees[4] = new Employee("a", "b", "c", 1, 42000);
 
 //        employees[2] = new Employee("a", "b", "c", 1, 35000);
 //        getEmployeesData(employees);
 //        countMonthSalaryExpenses(employees);
-//        System.out.println(findEmployeeMinSalary(employees));
+        System.out.println(findEmployeeMaxSalary(employees));
     }
     public static void getEmployeesData(Employee[] employeesList) {
         System.out.println("Личный состав:");
@@ -25,27 +25,41 @@ public class Main {
 
 
     public static int countMonthSalaryExpenses(Employee[] employeesList){
-        int monthSalary = 0;
+        int monthSalaryExpenses = 0;
         for (int i = 0; i < employeesList.length; i++) {
             if (employeesList[i] != null) {
-                monthSalary += employeesList[i].getSalary();
+                monthSalaryExpenses += employeesList[i].getSalary();
             }
         }
-        return monthSalary;
+        return monthSalaryExpenses;
         }
 
-    public static int findEmployeeMinSalary(Employee[] employeesList){
+    public static String findEmployeeMinSalary(Employee[] employeesList){
         int minSalary = 0;
+        String employeeWithMinSalary = "";
         for (int i = 0; i < employeesList.length; i++) {
             if(employeesList[i] != null && employeesList[i].getSalary() < minSalary || minSalary == 0 && employeesList[i] != null){
                 minSalary = employeesList[i].getSalary();
+                employeeWithMinSalary = employeesList[i].toString();
             }
             else{
 
             }
         }
-        return minSalary;
+        return employeeWithMinSalary;
 
+    }
+
+    public static String findEmployeeMaxSalary(Employee[] employeesList){
+        int maxSalary = 0;
+        String employeeWithMaxSalary = "";
+        for (int i = 0; i < employeesList.length; i++) {
+            if (employeesList[i] != null && employeesList[i].getSalary() > maxSalary){
+                maxSalary = employeesList[i].getSalary();
+                employeeWithMaxSalary = employeesList[i].toString();
+            }
+        }
+        return employeeWithMaxSalary;
     }
 //    -------------------------------------    class end -------------------------------------
 }
