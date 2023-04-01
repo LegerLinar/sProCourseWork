eb8824fe1b52ef8e6faf59277ee305d4d29787ce
pavelbesedin <pavelbesedin97@mail.ru>
2023-04-01 17:42:52 +0300

    Task 8 basic  subtask 2. Посчитать сумму затрат на зарплаты в месяц.

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,32 @@ public class Main {
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
         employees[1] = new Employee("a", "b", "c", 1, 35000);
-        getEmployeesData(employees);
+//        employees[2] = new Employee("a", "b", "c", 1, 35000);
+//        getEmployeesData(employees);
+        countMonthSalaryExpenses(employees);
+
     }
-    public static void getEmployeesData(Employee[] employeesList){
+    public static void getEmployeesData(Employee[] employeesList) {
         System.out.println("Личный состав:");
         for (int i = 0; i < employeesList.length; i++) {
-            if (employeesList[i] != null)
-            System.out.println(employeesList[i].toString());
+            if (employeesList[i] != null) {
+                System.out.println(employeesList[i].toString());
+            } else {
+                System.out.println("Вакантное место");
+            }
         }
     }
 
-}
+
+    public static void countMonthSalaryExpenses(Employee[] employeesList){
+        int monthSalary = 0;
+        for (int i = 0; i < employeesList.length; i++) {
+            if (employeesList[i] != null) {
+                monthSalary += employeesList[i].getSalary();
+            }
+        }
+            System.out.println("Затраты на зарплату сотрудникам в месяц - " + monthSalary + "руб.");
+        }
+        }
+
+
