bfd3e8a60178984777157b855ee4c0f3a46ab83e
pavelbesedin <pavelbesedin97@mail.ru>
2023-04-01 20:39:17 +0300

    Task 8  Subtask 6. Получить Ф. И. О. всех сотрудников (вывести в консоль).

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,17 @@ import pro.sky.java.course1.course.Employee;
 public class Main {
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
-        employees[0] = new Employee("a", "b", "c", 1, 35000);
-        employees[2] = new Employee("a", "b", "c", 1, 32000);
-        employees[4] = new Employee("a", "b", "c", 1, 42000);
+        employees[0] = new Employee("Кисложопкин", "Аркадий", "Васильевич", 1, 35000);
+        employees[2] = new Employee("Селиванов", "Акакий", "Александрович", 1, 32000);
+        employees[4] = new Employee("Кулиджи", "Казимир", "Космосович", 1, 42000);
 
-//        employees[2] = new Employee("a", "b", "c", 1, 35000);
-//        getEmployeesData(employees);
-//        countMonthSalaryExpenses(employees);
-        System.out.println(findEmployeeMaxSalary(employees));
+        showEmployeesData(employees);
+        System.out.println(countMonthSalaryExpenses(employees));
+        System.out.println(countAverageMonthSalary(employees));
+        showEmployeesNames(employees);
     }
-    public static void getEmployeesData(Employee[] employeesList) {
+
+    public static void showEmployeesData(Employee[] employeesList) {
         System.out.println("Личный состав:");
         for (int i = 0; i < employeesList.length; i++) {
             if (employeesList[i] != null) {
@@ -54,13 +55,37 @@ public class Main {
         int maxSalary = 0;
         String employeeWithMaxSalary = "";
         for (int i = 0; i < employeesList.length; i++) {
-            if (employeesList[i] != null && employeesList[i].getSalary() > maxSalary){
+            if (employeesList[i] != null && employeesList[i].getSalary() > maxSalary) {
                 maxSalary = employeesList[i].getSalary();
                 employeeWithMaxSalary = employeesList[i].toString();
             }
         }
         return employeeWithMaxSalary;
     }
+
+    public static int countAverageMonthSalary(Employee[] employeesList) {
+        int totalMonthSalary = 0;
+        int employeesCounter = 0;
+        for (int i = 0; i < employeesList.length; i++) {
+            if (employeesList[i] != null) {
+                totalMonthSalary += employeesList[i].getSalary();
+                employeesCounter++;
+            }
+        }
+        return totalMonthSalary / employeesCounter;
+    }
+
+    public static void showEmployeesNames(Employee[] employees) {
+        String employeesList = "";
+        for (int i = 0; i < employees.length; i++) {
+
+            if (employees[i] != null) {
+                employeesList += employees[i].getSurname() + " " + employees[i].getName() + " " + employees[i].getPatronymic() + "\n";
+            }
+        }
+        System.out.println(employeesList);
+    }
+
 //    -------------------------------------    class end -------------------------------------
 }
 
